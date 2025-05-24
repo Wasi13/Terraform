bd17850aa588242be91a35b8f6f001916f0cb9a0
Wasi13 <waseempashaglofficial@gmail.com>
2025-05-24 19:30:17 +0530

    Add files via upload

---
+++ MultiRegion_usingVar_OP_provider/output.tf
@@ -0,0 +1,16 @@
+output "use2_public_ip" {
+  value = aws_instance.use2_public.public_ip
+}
+
+output "use2_private_id" {
+  value = aws_instance.use2_private.id
+}
+
+output "usw2_public_ip" {
+  value = aws_instance.usw2_public.public_ip
+}
+
+output "usw2_private_id" {
+  value = aws_instance.usw2_private.id
+}
+
