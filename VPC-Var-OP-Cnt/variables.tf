0402ea3549e863d729cef2b42160d734ccecf578
Wasi13 <waseempashaglofficial@gmail.com>
2025-05-29 15:11:19 +0530

    Create variables.tf

---
+++ VPC-Var-OP-Cnt/variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.25.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.25.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  default = "10.25.2.0/24"
+}
+
+variable "key_name" {
+  default = "Key2"
+}
+
+variable "instance_count" {
+  default = 3
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    Environment = "Dev"
+    Project     = "Terraform-RedHat-Infra"
+  }
+}
