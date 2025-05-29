a6356b82bea8c71366ad865936ea30fa64c5d639
Wasi13 <waseempashaglofficial@gmail.com>
2025-05-29 18:02:41 +0530

    Create variable.tf

---
+++ S3_bucket_fileupload/variable.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "bucket_name" {
+  default = "my-failover-bucket-1303"
+}
+
+variable "local_file_path" {
+  default = "C:\Users\Admin\Downloads\failover.PNG" # Adjust for Windows or Mac
+}
