20435504d9e9717607c46a3bf70c09187a117246
Wasi13 <waseempashaglofficial@gmail.com>
2025-06-04 10:31:45 +0530

    Create variables.tf

---
+++ Project2/modules/vpc/variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name tag for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
+
+variable "public_az" {
+  description = "Availability Zone for the public subnet"
+  type        = string
+}
+
+variable "private_az" {
+  description = "Availability Zone for the private subnet"
+  type        = string
+}
